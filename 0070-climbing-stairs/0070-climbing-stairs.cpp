0c9e17061f3aea4c52e156652a8a3c0972619fc1
DHEERAJ BHAGAT <dheeraj.bhagat.work@gmail.com>
2026-08-11 10:56:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n <= 2)
+    int solve(int n, vector<int>& dp) {
+        if(n <= 2)
             return n;
-        int prev2 = 1;
-        int prev1 = 2;
-        for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+
+        if(dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
     }
 };
